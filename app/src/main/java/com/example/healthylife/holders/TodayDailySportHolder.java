027cbcf995060d3d24b915ab67468e0ce3fa74f4
U170709012 <kiymetderentoy@posta.mu.edu.tr>
2021-01-13 21:45:04 +0300

    sport page finished

--- app/src/main/java/com/example/healthylife/holders/TodayDailySportHolder.java
+++ app/src/main/java/com/example/healthylife/holders/TodayDailySportHolder.java
@@ -23,7 +23,7 @@ public class TodayDailySportHolder {
         return this.todayDailySport.getTodaySports();
     }
 
-
+asdadadad
     public void addSport(Sport sport) {
         this.todayDailySport.addSport(sport);
     }
